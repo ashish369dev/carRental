b3f4dd1166804fb02fd35ca170ec6c9f2da41d1b
ashish369dev <ashish369shah@gmail.com>
2019-11-09 18:16:22 +0530

    Car rent calculate .. Submit validation pending

--- src/components/RentForm.vue
+++ src/components/RentForm.vue
@@ -1,85 +1,153 @@
 <template>
-<div class="section">
-    <div class="container ">
-        <h1 class="title ">
-            Car On Rent
-        </h1>
-        <p class="subtitle">
-            Drive when you want !!
-        </p>
-        <div class="column is-half is-offset-3">
-            <form action="" class="form ">
-                <b-field label="Name">
-                    <b-input v-model="name"></b-input>
-                </b-field>
-                <b-field label="Email">
-                    <b-input type="email" placeholder="Enter email"></b-input>
-                </b-field>
-                <b-field grouped>
-                    <b-field label="Contact" expanded>
-                        <b-field>
-                            <b-select v-model="ext" placeholder="Ext.">
-                                <option selected value="+91">+91</option>
-                                <option value="+44">+44</option>
-                                <option value="+11">+11</option>
-                            </b-select>
-                            <b-input type="text" pattern="[0-9]*" v-model="contact"
-                                validation-message=" Enter Numbers Only" placeholder="Enter Contact" expanded>
-                            </b-input>
-                        </b-field>
-                    </b-field>
-                </b-field>
-                <b-field label="Rent For" expanded></b-field>
-                <b-field>
-                    <b-radio-button v-model="rentday" native-value="one">
-                        <b-icon icon="calendar-today" size="is-small" type="is-success"> </b-icon>
-                        <span>One Day</span>
-                    </b-radio-button>
-                    <b-radio-button v-model="rentday" native-value="multiple">
-                        <span>Multiple Day</span>
-                    </b-radio-button>
-                </b-field>
-                <b-field label="Car Group">
-                    <b-select placeholder="Select car group" expanded>
-                        <option value="Hatchback">Hatchback</option>
-                        <option value="Sedan">Sedan</option>
-                        <option value="SUV">SUV</option>
-                        <option value="XUV">XUV</option>
-                    </b-select>
-                </b-field>
-                <b-field label="Car ">
-                    <b-select placeholder="Select car " expanded>
-                        <option value="Hatchback">Maruti Swift AT </option>
-                        <option value="Hatchback">Tata Tiago </option>
-                        <option value="Sedan">Hyundai Verna</option>
-                        <option value="SUV">Ford Ecosport </option>
-                        <option value="XUV">Audi Q3</option>
-                    </b-select>
-                </b-field>
-                <b-field label="Select Date" v-if="rentday=='multiple'">
-                    <b-datepicker placeholder="Select Date" v-model="dates" range></b-datepicker>
-                </b-field>
-                <b-field label="Select Date" v-else-if="rentday=='one'">
-                    <b-datepicker placeholder="Select Date" v-model="date"></b-datepicker>
-                </b-field>
-            </form>
-        </div>
+  <div class="section">
+    <div class="container">
+      <h1 class="title">Car On Rent</h1>
+      <p class="subtitle">Drive when you want !!</p>
+      <div class="column is-half is-offset-3">
+        <form action class="form" @submit.prevent="submitForm">
+          <b-field label="Name">
+            <b-input v-model="name" placeholder="Enter Name"></b-input>
+          </b-field>
+          <b-field label="Email">
+            <b-input type="email" placeholder="Enter email"></b-input>
+          </b-field>
+          <b-field grouped>
+            <b-field label="Contact" expanded>
+              <b-field>
+                <b-select v-model="ext" placeholder="Ext.">
+                  <option v-for="data in contacts" :key="data.id" :value="data.value">{{data.value}}</option>
+                </b-select>
+                <b-input
+                  type="text"
+                  pattern="[0-9]*"
+                  v-model="contact"
+                  validation-message=" Enter Numbers Only"
+                  placeholder="Enter Contact"
+                  expanded
+                ></b-input>
+              </b-field>
+            </b-field>
+          </b-field>
+          <b-field label="Rent For" expanded></b-field>
+          <b-field>
+            <b-radio-button v-model="rentday" native-value="one">
+              <span>One Day</span>
+            </b-radio-button>
+            <b-radio-button v-model="rentday" native-value="multiple">
+              <span>Multiple Day</span>
+            </b-radio-button>
+          </b-field>
+          <b-field label="Car Type">
+            <b-select placeholder="Select car group" expanded v-model="carType">
+              <option v-for="data in carTypes" :key="data.id" :value="data.value">{{data.value}}</option>
+            </b-select>
+          </b-field>
+          <b-field label="Car">
+            <b-select placeholder="Select car " expanded v-model="car">
+              <template v-if="carType">
+                <template v-for="item in cars">
+                  <option
+                    v-if="item.type==carType"
+                    :key="item.id"
+                    :value="item.value"
+                  >{{item.value}}</option>
+                </template>
+              </template>
+              <template v-else-if="!carType">
+                <option></option>
+              </template>
+            </b-select>
+          </b-field>
+          <div class="columns">
+            <b-field class="column is-6" v-if="rent">Per day rent : &#36;{{rent}}</b-field>
+            <b-field class="column is-6 label" v-if="total">Total : &#36;{{total}}</b-field>
+          </div>
+          <b-field label="Select Date" v-if="rentday=='multiple'">
+            <b-datepicker placeholder="Select Date" v-model="dates" range></b-datepicker>
+          </b-field>
+          <b-field label="Select Date" v-else-if="rentday=='one'">
+            <b-datepicker placeholder="Select Date" v-model="date"></b-datepicker>
+          </b-field>
+          <b-button native-type="submit" class="is-primary">submit</b-button>
+        </form>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "RentForm",
-  data () {
-      return {
-            name: '',
-            contact: '',
-            ext: null,
-            rentday: 'one',
-            dates: [],
-            date: null
-        }
+  data() {
+    return {
+      name: "",
+      contact: "",
+      ext: null,
+      rentday: "one",
+      dates: [],
+      date: null,
+      carType: null,
+      car: null,
+      rent: null,
+      total: 0,
+      contacts: [{ value: "+91" }, { value: "+44" }, { value: "+40" }],
+      carTypes: [
+        { value: "Hatchback" },
+        { value: "Sedan" },
+        { value: "SUV" },
+        { value: "XUV" }
+      ],
+      cars: [
+        { type: "Hatchback", value: "Maruti Swift AT", perday: 1000 },
+        { type: "Hatchback", value: "Tata Tiago", perday: 1800 },
+        { type: "Sedan", value: "Hyundai Verna", perday: 2100 },
+        { type: "SUV", value: "Ford Ecosport", perday: 2500 },
+        { type: "XUV", value: "Audi Q3", perday: 3000 }
+      ]
+    };
+  },
+  watch: {
+    carType: function() {
+      this.rent = null;
+      this.total = 0;
+      this.dates = null;
+      this.date = null;
+    },
+    car: function(val) {
+      let car = this.cars.find(item => item.value === val);
+      this.rent = car.perday;
+      this.total = this.rent;
+      if (this.dates) {
+        this.dates = null;
+      } else if (this.date) {
+        this.date = null;
+      }
+    },
+    dates: function(val) {
+      if (val) {
+        var date1 = new Date(val[0]);
+        var date2 = new Date(val[1]);
+        var Difference_In_Time = date2.getTime() - date1.getTime();
+        var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+        this.calculateTotal(Difference_In_Days + 1);
+      }
+    },
+    date: function(val) {
+      if (val) {
+        this.calculateTotal(1);
+      }
+    },
+    rentday: function() {
+      this.total = 0;
+      this.dates = this.date = null;
     }
+  },
+  methods: {
+    calculateTotal(days) {
+      let car = this.cars.find(item => item.value === this.car);
+      this.total = days * car.perday;
+    },
+    submitForm() {}
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import buefy from 'buefy'
-import 'buefy/dist/buefy.css'
-import 'bulma/css/bulma.css'
+import Vue from 'vue';
+import App from './App.vue';
+import buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import 'bulma/css/bulma.css';
+import { validate } from 'vee-validate';
 
 Vue.config.productionTip = false
-Vue.use(buefy)
+Vue.use(buefy);
+// Register it globally
+Vue.component(validate);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
